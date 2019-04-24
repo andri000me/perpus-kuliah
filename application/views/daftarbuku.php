b544f268b25903142e3eec7c7e9a3773f295985b
Khusaeriz <akhus22@gmail.com>
2019-04-24 16:14:04 +0700

    change btn style

--- application/views/daftarbuku.php
+++ application/views/daftarbuku.php
@@ -1,6 +1,6 @@
 <html lang="en">
 	<head>
-		<title>Perpustakaan |</title>
+		<title>Perpustakaan | Daftar Buku</title>
 		<link rel="stylesheet" href="<?php echo base_url()?>assets/css/bootstrap.min.css" />
 		<script src="<?php echo base_url()?>assets/js/jquery.min.js"></script>
 		<script src="<?php echo base_url()?>assets/js/bootstrap.min.js"></script>
@@ -30,7 +30,7 @@
 					<!-- looping products -->
 					  <?php foreach($buku as $buku) { ?>
 					  <div class="col-sm-3 col-md-3">
-						<div class="thumbnail" style="height: 370px;">
+						<div class="thumbnail" style="height: 390px;">
 							<img src="<?php echo base_url();?>assets/upload/<?=$buku->gambar;?>" style="max-width:100%; max-height: 100%; height: 150px; width: 120px">
 
 						  <div class="caption">
@@ -49,7 +49,7 @@
 										'role'	=> 'button',
 									]);
 								} else {
-									echo "<button class='btn btn-primary' disabled='true'>Buku Habis</button>";
+									echo "<button class='btn btn-danger' disabled='true'>Buku Habis</button>";
 								}
 								?>
 								<?=anchor('buku/katalog_detail/' . $buku->id_buku, ' Detail' , [
